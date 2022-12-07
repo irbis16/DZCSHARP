4005c369343cb30b99bbd8af6b337b70592f45a0
irbis16 <irbis16@yandex.ru>
2022-12-07 18:46:10 +0300

    ДЗ 27

---
+++ DZ27/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+int N;
+int sum = 0;
+Console.Write ($"Введите число от 1:");
+int.TryParse(Console.ReadLine()!, out N);
+for (int i = 0; i <= N; i++)
+sum = sum + i;
+
+Console.Write ($"{sum} ");
