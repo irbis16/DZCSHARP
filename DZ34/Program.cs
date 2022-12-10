5fadab70343e2c730a0e8a7fdb71f680b34c6d85
irbis16 <irbis16@yandex.ru>
2022-12-10 23:37:41 +0300

    Задание 34

---
+++ DZ34/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+int[] array = new int[3];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 == 0)
+        k++;
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+Console.Write($"-->{k}");
