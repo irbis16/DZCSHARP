13aa9ef2dbdbd9a6452b4239c73f153538a51f36
irbis16 <irbis16@yandex.ru>
2022-12-13 15:06:55 +0300

    DZ41

---
+++ DZ41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//Console.WriteLine("Введите элементы массива через пробел");
+Console.Write("введите числа массива через пробел:\n");
+string[] numbers = Console.ReadLine()!.Split(' ');
+int[] array = new int[numbers.Length];
+Console.Write($"Ваш массив:\n");
+for (int i = 0; i < numbers.Length; i++)
+{
+    int.TryParse(numbers[i], out array[i]);
+    Console.Write($"{array[i]} ");
+}
+
+int k = 0;
+foreach (var i in array)
+    if (i > 0)
+        k++;
+Console.Write($"\n{k} число больше 0");
+
