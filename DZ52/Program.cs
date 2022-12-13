88272d2f0b9298f206dde2cfd7e53f6fd7d7661a
irbis16 <irbis16@yandex.ru>
2022-12-14 00:40:58 +0300

    ДЗ 52

---
+++ DZ52/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+
+int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write("{0,3}", $" {array[i, j]}");
+    }
+    Console.WriteLine();//переход на новую строку
+}
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($" {Math.Round((double)sum / array.GetLength(0), 1)}");
+    //Console.WriteLine();//переход на новую строку
+}
