421d86f4c6e196a744bbfb88cb5e23672e56e87b
irbis16 <irbis16@yandex.ru>
2022-12-18 19:31:36 +0300

    DZ64

---
+++ DZ64/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+
+int n = 10;
+Console.Write($"{PrintNums(n, 1)}");
+string PrintNums(int left, int right)
+{
+    if (left <= right)
+        return left.ToString();
+    return $"{left} {PrintNums(left - 1, right)}";
+}
