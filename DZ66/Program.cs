10e63d864c554d867b54a24684fa497ba20f38d6
irbis16 <irbis16@yandex.ru>
2022-12-18 19:33:03 +0300

    DZ66

---
+++ DZ66/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+int m = 1, n = 15;
+Console.Write($"{Summ(m, n)}");
+int Summ(int left, int right)
+{
+    if (left >= right)
+        return left;
+    return left + Summ(left + 1, right);
+}
